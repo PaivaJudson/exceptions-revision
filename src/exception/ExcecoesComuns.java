c9580f919479ac0ce8aa9465e9b5558d41ac2c4c
judsonpaiva <judson.paiva@hotmail.com>
2022-04-06 07:57:56 +0100

    exceptions comuns

---
+++ src/exception/ExcecoesComuns.java
@@ -0,0 +1,23 @@
+package exception;
+
+public class ExcecoesComuns {
+	
+	static int[] arrayNull = new int[0];
+	
+	public static void main(String[] args) {
+		
+		//NullPointerException
+		//System.out.println(arrayNull.length);
+		
+		//ArithmeticException
+		//int x = 5 / 0;
+		
+		//ArrayIndexOutOfBoundsException
+		//System.out.println(arrayNull[1]);
+		
+		//ClassCastException
+		
+		//NumberFormatException
+		
+	}
+}
