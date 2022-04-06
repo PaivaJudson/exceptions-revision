4452e077cc0ed914190dbd85054d8b2e1b3e8d34
judsonpaiva <judson.paiva@hotmail.com>
2022-04-06 07:48:28 +0100

    cláusula throws

--- src/exception/DividirPorZero.java
+++ src/exception/DividirPorZero.java
@@ -4,21 +4,29 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DividirPorZero {
-
+	
+	public static void dividir(Scanner s) throws InputMismatchException, ArithmeticException {
+		
+		System.out.print("Informar  valor: ");
+		int a = s.nextInt();
+		
+		System.out.print("Informar outro valor: ");
+		int b = s.nextInt();
+		
+		System.out.println("Resultado = "+ a / b);	
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
 		boolean continua = true;
 		
+		
+		
 		do {
 			try{
-				System.out.print("Informar  valor: ");
-				int a = s.nextInt();
-				
-				System.out.print("Informar outro valor: ");
-				int b = s.nextInt();
-				
-				System.out.println("Resultado = "+ a / b);
+				dividir(s);
 				
 				continua = false;
 				
