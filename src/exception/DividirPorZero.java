d33962b1f6ab8d7263cdee19839151e9663d4ed8
judsonpaiva <judson.paiva@hotmail.com>
2022-04-06 07:27:05 +0100

    finally

--- src/exception/DividirPorZero.java
+++ src/exception/DividirPorZero.java
@@ -1,5 +1,6 @@
 package exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DividirPorZero {
@@ -7,13 +8,30 @@ public class DividirPorZero {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		System.out.print("Informar  valor: ");
-		int a = s.nextInt();
+		boolean continua = true;
 		
-		System.out.print("Informar outro valor: ");
-		int b = s.nextInt();
-		
-		System.out.println("Valor = "+ a / b);
-	}
-
+		do {
+			try{
+				System.out.print("Informar  valor: ");
+				int a = s.nextInt();
+				
+				System.out.print("Informar outro valor: ");
+				int b = s.nextInt();
+				
+				System.out.println("Resultado = "+ a / b);
+				
+				continua = false;
+				
+			}catch(ArithmeticException er) {
+				System.err.println("o divisor não deve ser nulo...");
+				
+			}catch(InputMismatchException er) {
+				System.err.println("Os números devem ser inteiros");
+			}finally {
+				s.nextLine();
+				System.out.println("Finally executado...");
+			}
+				
+		}while(continua);		
+	}	
 }
